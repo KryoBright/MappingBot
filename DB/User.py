c23e30c99711c593a2959c61d8d1d7022844faaa
MarkKislov <51121084+MarkKislov@users.noreply.github.com>
2020-06-13 22:28:28 +0700

    Adding sponsors and their data to the DB works
    It is better to work out operations with sponsor points.

--- DB/User.py
+++ DB/User.py
@@ -8,6 +8,17 @@ class User:
 	lat = None
 	lon = None
 
+	balance = 0
+
+
+class SponsorPoint:
+	telegrammUserId = None
+	lat = None
+	lon = None
+
+
+
+
 def addUser(user):
 	query = Template("CREATE (N:USER {telegrammUserId:'$telegrammUserId',roomId:'$roomId', roomName:'$roomName', lat:'$lat', lon:'$lon'})")
 	session.run(query.substitute(telegrammUserId=user.telegrammUserId, roomId=user.roomId, roomName=user.roomName, lat=user.lat, lon=user.lon))
@@ -19,19 +30,35 @@ user.roomName = 'Ivan'
 user.lat = 324.3243
 user.lon = -43.342
 
-addUser(user)
+#addUser(user)
 
 
 def userPropUpdate(telegrammUserId, prop, value):
 	query = Template("MATCH (x:USER) where x.telegrammUserId='$telegrammUserId' SET x.$prop = '$value'  return (x)")
 	session.run(query.substitute(telegrammUserId=telegrammUserId, prop=prop, value=value))
 
-userPropUpdate(246375635543, 'roomName', 'mark')
+#userPropUpdate(246375635543, 'roomName', 'mark')
+
+
+def addSponsor(telegrammUserId):
+	query = Template("MATCH (x:USER) where x.telegrammUserId='$telegrammUserId' SET x: SPONSOR SET x.balance=0 return (x)")
+	session.run(query.substitute(telegrammUserId=telegrammUserId))
+
+
+def addSonsorPoint(telegrammUserId, lat, lon):
+	query = Template("MATCH (SPONSOR:SPONSOR) WHERE SPONSOR.telegrammUserId='$telegrammUserId' CREATE (x:SPONSOR_POINT {telegrammUserId:'$telegrammUserId', lat:'$lat', lon:'$lon'}) CREATE (SPONSOR_POINT)-[r:Принадлежит]->(SPONSOR)")
+	session.run(query.substitute(telegrammUserId=user.telegrammUserId,lat=user.lat, lon=user.lon))
+
+addSonsorPoint(246375635543, 23, 365)
+#addSponsor(246375635543)
 
 def getUsers():
 	return session.run("MATCH (x:USER) return (x)")
 
+def getSponsors():
+	return session.run("MATCH (x:SPONSOR) return (x)")
 
-users = getUsers()
+
+users = getSponsors()
 for user in users:
 	print(user)
