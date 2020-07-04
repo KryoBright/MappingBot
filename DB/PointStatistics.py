5654cc8ad616eacf60ada7535fca336de1beef44
MarkKislov <51121084+MarkKislov@users.noreply.github.com>
2020-07-04 17:08:18 +0700

    Find nearest sponsor point to the meeting point
    A function has been created that accepts a meeting point and returns the nearest sponsor point to this meeting point.
    
    Sponsorship points are taken from the database

--- DB/PointStatistics.py
+++ DB/PointStatistics.py
@@ -17,3 +17,53 @@ def addStockPoint(latitude, longitude):
 def removeStockPoint(latitude, longitude):
 	query = Template("MATCH (x:StockPoint) WHERE x.latitude='$latitude' AND x.longitude='$longitude' DELETE (x)")
 	session.run(query.substitute(latitude=latitude, longitude=longitude))
+
+
+def removeStockPoint(latitude, longitude):
+	query = Template("MATCH (x:StockPoint) WHERE x.latitude='$latitude' AND x.longitude='$longitude' DELETE (x)")
+	session.run(query.substitute(latitude=latitude, longitude=longitude))
+
+
+
+class SponsorPoint:
+	def __init__(self):
+		self.SponsorProfile = None
+		self.name = ""
+		self.about = ""
+		self.image = None
+		self.latitude = 0
+		self.longitude = 0
+
+
+def getNearestSponsorPoint(point):
+	sponsorPoints = []
+	arr = session.run("MATCH (x:SPONSOR_POINT) return x.SponsorProfile AS SponsorProfile, x.about AS about, x.name AS name, x.latitude AS latitude, x.longitude as longitude")
+	for p in arr:
+		sponsorPoint = SponsorPoint()
+		sponsorPoint.SponsorProfile = p['SponsorProfile']
+		sponsorPoint.name = p['name']
+		sponsorPoint.about = p['about']
+		sponsorPoint.image = p['image']
+		sponsorPoint.latitude = p['latitude']
+		sponsorPoint.longitude = p['longitude']
+
+		sponsorPoints.append(sponsorPoint)
+
+	if(len(sponsorPoints) == 0):
+		return 'no'
+
+	S = 1000000000
+	res = sponsorPoints[0]
+	for p in sponsorPoints:
+		s = ((p.latitude - point.latitude) ** 2) + ((p.latitude - point.latitude) ** 2)
+		if(s < S):
+			S = s
+			res = p
+
+	return p
+
+
+point = SponsorPoint()
+point.latitude = 47
+point.longitude = 82
+print(getNearestSponsorPoint(point))
